46beeac511d078a31b917ca70011714ce0726375
Canquito <jbejarano@uniempresarial.edu.co>
2024-12-02 11:07:57 -0500

    New Changes
    Creacion de Interfaces y Clases

---
+++ src/main/java/co/edu/ue/jpa/IBoletasJpa.java
@@ -0,0 +1,9 @@
+package co.edu.ue.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.ue.entity.Boletas;
+
+public interface IBoletasJpa extends JpaRepository<Boletas, Integer>{
+
+}
---
+++ src/main/java/co/edu/ue/jpa/IFuncionesJpa.java
@@ -0,0 +1,9 @@
+package co.edu.ue.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.ue.entity.Funciones;
+
+public interface IFuncionesJpa extends JpaRepository<Funciones, Integer>{
+
+}
